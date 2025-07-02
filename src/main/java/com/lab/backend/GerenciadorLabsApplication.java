af0f45fd5cdc9c588cfbf92042cd6ea0886a1ca1
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2025-07-02 15:10:29 -0300

    initial config

---
+++ src/main/java/com/lab/backend/GerenciadorLabsApplication.java
@@ -0,0 +1,13 @@
+package com.lab.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLabsApplication.class, args);
+	}
+
+}
